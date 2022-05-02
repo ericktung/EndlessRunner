f9eeda51d428919b00261af16d7f359e4cc98d72
spandemic <anloeng@ucsc.edu>
2022-05-02 10:58:49 -0700

    Rubric addition

--- src/main.js
+++ src/main.js
@@ -1,3 +1,18 @@
+/*
+Heart Breakout!
+Created by: Andy Eng, Ashley Lu, Erick Tung, Siwen Tao
+Completed: 5/1/2022
+
+Creative Tilt:
+Does the game do something technically interesting? 
+We believe the logic for generating platforms and obstacles justifies the points in this category.
+Obstacle logic: Play.js line 395
+Two-hit death logic: Play.js line 413
+
+Does the game have a great visual style?
+The artwork in the game is high quality.
+*/
+
 let cursors;
 let currentScene = 0;
 const SCALE = 0.5;
